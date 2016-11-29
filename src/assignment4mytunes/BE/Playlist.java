010eafbd8c11ae71ac15e6ffe536fe85825d4fdb
Gudlauggu <gudl0025@easv365.dk>
2016-11-29 12:17:15 +0100

    Playlist
    added the getter and setter and more to the Playlist

--- src/assignment4mytunes/BE/Playlist.java
+++ src/assignment4mytunes/BE/Playlist.java
@@ -5,11 +5,40 @@
  */
 package assignment4mytunes.BE;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author gudla
  */
 public class Playlist
 {
+    private String name;
+    private ArrayList<Music> playlist;
+
+    public Playlist(String name)
+      {
+        this.name = name;
+      }
+
+    public String getName()
+      {
+        return name;
+      }
+
+    public void setName(String name)
+      {
+        this.name = name;
+      }
+    
+    public void addSong(Music song)
+      {
+        playlist.add(song);
+      }
+
+    public ArrayList<Music> getPlaylist()
+      {
+        return playlist;
+      }
     
 }
